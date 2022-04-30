2675994610383632e8a4d12edacd5a782bcafc85
suske <suske@s19.be>
2022-04-30 19:59:58 +0200

    test main

--- main.cpp
+++ main.cpp
@@ -57,6 +57,29 @@ int		main(int argc, char **argv) {
 		return 1;
 	}
 
+	Client client(4);
+	strncpy(client.getBuf(), "NICK suske\r\n", 512);
+	client.parse_input();
+	//std::cout << db->next_response().second << std::endl;
+	strncpy(client.getBuf(), "USER suske suske * :suske\r\n", 512);
+	client.parse_input();
+	//std::cout << db->next_response().second << std::endl;
+	strncpy(client.getBuf(), "PASS abc\r\n", 512);
+	client.parse_input();
+	//std::cout << db->next_response().second << std::endl;
+	strncpy(client.getBuf(), "JOIN #abc\r\n", 512);
+	client.parse_input();
+	std::cout << db->responses.size() << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	std::cout << db->next_response().second << std::endl;
+	return 0;
 	do_main(argc, argv);
 	return 0;
 }
